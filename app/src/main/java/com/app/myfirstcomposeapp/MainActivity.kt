0445816356b95677d925786b985b4d25ba8f57bb
mariusfacepoze <stroescump@gmail.com>
2022-01-22 20:00:23 +0200

    initial commit - created project

---
+++ app/src/main/java/com/app/myfirstcomposeapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.app.myfirstcomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.app.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyFirstComposeAppTheme {
+                // A surface container using the 'background' color from the theme
+                PlaceholderText()
+            }
+        }
+    }
+}
+
+@Composable
+private fun PlaceholderText() {
+    Surface(color = MaterialTheme.colors.background) {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyFirstComposeAppTheme {
+        PlaceholderText()
+    }
+}
